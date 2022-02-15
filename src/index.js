c0982b3fb83a2919efa6deb8ade75e28ce782498
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-02-15 15:35:04 -0800

    commit tree c184ec8eb3f7c7ca531ff06bda77ad526e385da3

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// Step 1. Import react-router functions
+import { BrowserRouter, Route , Routes} from "react-router-dom";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
 
-function Home() {
+
+// Step 2. Use <Route> components to define client-side routes in our app
+function App() {
   return (
     <div>
-      <h1>Home!</h1>
+      <NavBar />
+      <Routes>
+        <Route path="/about" element = {<About />} />     
+        <Route path="/login" element={<Login />}/> 
+        <Route exact path="/" element={<Home />} />     
+      </Routes>
     </div>
+  
+    
   );
 }
 
-function App() {
-  return <Home />;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+// Step 3. Use <BrowserRouter> component to wrap the entire application and provide React Router context features
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
